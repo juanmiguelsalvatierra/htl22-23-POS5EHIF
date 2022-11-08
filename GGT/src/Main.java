e98623484cbaf6b01beaafc5d7096bb79eb86b9c
Juan Miguel Salvatierra <ignitor31@icloud.com>
2022-11-08 14:45:56 +0100

    IsPrim

--- GGT/src/Main.java
+++ GGT/src/Main.java
@@ -1,14 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        /*System.out.println(ggt(15,20));
+        static Random rnd = new Random();
+        System.out.println("----------------GGT----------------");
+        System.out.println(ggt(15,20));
         System.out.println(ggt(24, 16));
-        System.out.println(ggt(25,16));*/
+        System.out.println(ggt(25,16));
+        System.out.println("----------------Potenz_mod_n----------------");
         System.out.println(potenz_mod_n(2,0,5));
         System.out.println(potenz_mod_n(2,1,5));
         System.out.println(potenz_mod_n(2,2,5));
         System.out.println(potenz_mod_n(3,128,5));
         System.out.println(potenz_mod_n(2,128,5));
         System.out.println(potenz_mod_n(3,3,5));
+        System.out.println("----------------IsPrim----------------");
+        System.out.println(isPrim(19));
+        System.out.println(isPrim(9973));
+        System.out.println(isPrim(65537));
+        System.out.println(isPrim(123));
+        System.out.println(isPrim(58081));
+        System.out.println(isPrim(63001));
+        System.out.println(isPrim(65535));
     }
 
     public static int ggt(int a, int b){
@@ -27,5 +38,24 @@ public class Main {
             erg = erg % n;
         }
         return erg;
+    }
+
+    public static boolean isPrim(long n){
+        long a = rnd.next(1, n);
+        if(n % 2 == 0){
+            return false;
+        }
+        if(ggt(a,n) > 1){
+            return false;
+        }
+        boolean temp = false;
+        for(int i = 0; i < 20; i++) {
+            long b = potenz_mod_n(a, (n-1)/2, n);
+            if (b == 1 || ) {
+                temp = true;
+            } else {
+                temp = false;
+            }
+        }
     }
 }
