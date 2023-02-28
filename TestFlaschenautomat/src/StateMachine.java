68a78557077bd725cd81c875f8924b028f0d278b
Juan Miguel Salvatierra <31824478+juanmiguelsalvatierra@users.noreply.github.com>
2023-02-28 23:00:47 +0100

    HashMap FSM

--- TestFlaschenautomat/src/StateMachine.java
+++ TestFlaschenautomat/src/StateMachine.java
@@ -1,14 +1,33 @@
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class StateMachine implements IEingabe {
     IAusgabe aut;
     LinkedList<LinkedList<EZ>> states = new LinkedList<>();
+    HashMap<EZustand, HashMap<EEingabe, EZustand>> hashstates = new HashMap<>();
+    int currentstate = 0;
     public StateMachine(IAusgabe aut){
         this.aut = aut;
 
         for (int i = 0; i < 10; i++) {
             states.add(new LinkedList<>());
         }
+        hashstates.put(EZustand.Start, new HashMap<>());
+        hashstates.put(EZustand.Z1, new HashMap<>());
+        hashstates.put(EZustand.Z2, new HashMap<>());
+        hashstates.put(EZustand.Z3, new HashMap<>());
+        hashstates.put(EZustand.Z4, new HashMap<>());
+        hashstates.put(EZustand.Z5, new HashMap<>());
+        hashstates.put(EZustand.Z6, new HashMap<>());
+        hashstates.put(EZustand.Zhoch, new HashMap<>());
+        hashstates.put(EZustand.ZF, new HashMap<>());
+        hashstates.put(EZustand.ZR, new HashMap<>());
+
+        hashstates.get(EZustand.Start).put(EEingabe.Eins, EZustand.Z1);
+        hashstates.get(EZustand.Start).put(EEingabe.Zwei, EZustand.Z2);
+        hashstates.get(EZustand.Start).put(EEingabe.Knopf, EZustand.Z1);
+
+        hashstates.get(EZustand.Z1).put(EEingabe.Knopf, EZustand.Z1);
 
         states.get(0).add(new EZ(EEingabe.Eins, EZustand.Z1));
         states.get(0).add(new EZ(EEingabe.Zwei, EZustand.Z2));
@@ -52,6 +71,12 @@ public class StateMachine implements IEingabe {
     }
     @Override
     public void Eingabe(EEingabe eingabe) {
-
+        for(EZ item : states.get(currentstate)){
+            if(item.eing.equals(eingabe)){
+                aut.Ausgabe(item.zust);
+                break;
+                currentstate =
+            }
+        }
     }
 }
